e90eb432bac046e52e1ad20889bafc53d58029b6
Viliadie <96015763+Viliadie@users.noreply.github.com>
2023-01-08 16:15:51 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+/*
+    Авторизация
+ */
+
+$('.login_btn').click(function (e) {
+    e.preventDefault();
+
+    $(`input`).removeClass('error');
+
+    let login = $('input[name="login"]').val(),
+        password = $('input[name="password"]').val();
+
+    $.ajax({
+        url: 'vendor/signin.php',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            login: login,
+            password: password
+        },
+        success (data) {
+
+            if (data.status) {
+                document.location.href = '/profile.php';
+            } else {
+
+                if (data.type === 1) {
+                    data.fields.forEach(function (field) {
+                        $(`input[name="${field}"]`).addClass('error');
+                    });
+                }
+
+                $('.msg').removeClass('none').text(data.message);
+            }
+
+        }
+    });
+
+});
+
+
+/*
+    Регистрация
+ */
+
+$('.register_btn').click(function (e) {
+    e.preventDefault();
+
+    $(`input`).removeClass('error');
+
+    let login = $('input[name="login"]').val(),
+        password = $('input[name="password"]').val(),
+        email = $('input[name="email"]').val(),
+        password_confirm = $('input[name="password_confirm"]').val();
+
+    let formData = new FormData();
+    formData.append('login', login);
+    formData.append('password', password);
+    formData.append('password_confirm', password_confirm);
+    formData.append('email', email);
+
+
+    $.ajax({
+        url: 'vendor/signup.php',
+        type: 'POST',
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        cache: false,
+        data: formData,
+        success (data) {
+
+            if (data.status) {
+                document.location.href = '/index.php';
+            } else {
+
+                if (data.type === 1) {
+                    data.fields.forEach(function (field) {
+                        $(`input[name="${field}"]`).addClass('error');
+                    });
+                }
+
+                $('.msg_reg').removeClass('none').text(data.message);
+
+            }
+
+        }
+    });
+
+});
